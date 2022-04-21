25ba3b38b76dec990a587347e5c9a9b63ddaa740
internship311 <techtosh588@gmail.com>
2022-04-21 17:41:59 +0530

    creation of authentication page component alongside sign in form

--- crwn-clothing-shop/src/routes/authentication/authentication.component.jsx
+++ crwn-clothing-shop/src/routes/authentication/authentication.component.jsx
@@ -5,35 +5,31 @@ import { getRedirectResult } from 'firebase/auth';
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase.utils';
 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
-const SignIn = () => {
+import './authentication.styles.scss';
 
-    const fetchDataFromFirebaseRedirect = async() => {
+const Authentication = () => {
+
+    /* const fetchDataFromFirebaseRedirect = async() => {
         const response = await getRedirectResult(auth);
         //console.log(response);
         if(response) {
             const userDocRef = await createUserDocumentFromAuth(response.user);
         }
-    }
+    } */
 
-    useEffect(() => {
+    /* useEffect(() => {
         fetchDataFromFirebaseRedirect();
-    }, [])
-
-    const logGoogleUser = async () => {
-        const { user } =  await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-        //console.log(user);
-    }
+    }, []) */
 
     return(
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button>
+        <div className='authentication-container'>
+            <SignInForm />
+            {/* <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button> */}
             <SignUpForm />
         </div>
     )
 }
 
-export default SignIn;
+export default Authentication;
